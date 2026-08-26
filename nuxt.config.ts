60bed969bf38eb07eebd16a86b060e683e91e29a
frankizaw <franek.zawadzki@gmail.com>
2026-08-26 18:33:08 +0200

    fix(vite): allow all hosts for dev tunnels

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  vite: {
+    server: {
+      allowedHosts: true
+    }
+  }
 })
